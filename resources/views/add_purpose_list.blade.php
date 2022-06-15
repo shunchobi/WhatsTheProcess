cd50b86a093d4f606a4641611e5999f2c354dc19
AzureAD\近藤駿一 <kondo@qumu.co.jp>
2022-06-16 00:30:12 +0900

    step 1 to add new process list

--- resources/views/add_purpose_list.blade.php
+++ resources/views/add_purpose_list.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 
+<?php
+
+$current_sort_num = 1;
+
+?>
+
 <h1 class="add-purpose-title"> Purposeを追加する</h1>
 
 <div class="purpose-title">
@@ -15,13 +21,17 @@
 <p class="description-title-txt">説明</p>
 </div>
 
+<!-- 入力されたデータはどんな形（配列なのか？）でinsertできるか調べ、よい形のデータにまとめながら下記PHPで
+きれいに表現する -->
+<?php for($i = 0; $i < $current_sort_num; $i++) {?>
 <div class="processes-contents">
-<label class="sort-number">1</label>
-<input class="process-title" type="text" value="">
-<input class="process-command" type="text" value="">
-<input class="process-description" type="text" value="">
-<button class="delete-added-process-btn" type="button">削除</button>
+    <label class="sort-number"><?php $current_sort_num ?></label>
+    <input class="process-title" type="text" value="">
+    <input class="process-command" type="text" value="">
+    <input class="process-description" type="text" value="">
+    <button class="delete-added-process-btn" type="button">削除</button>
 </div>
+<?php } ?>
 
 <div class="add-new-process">
     <button class="add-new-process-btn" type="button">新しい工程を追加</button>
@@ -35,15 +45,30 @@
 
 <script>
 window.addEventListener('DOMContentLoaded', function () { 
+    //
+    // OKボタン
+    // 入力されたすべての情報をデータベースへinsertする
+    //
     $(".submit-btn").click(function(){
         const purpose_title = $(".purpose-title").val();
         const sort_number = parseInt($(".sort-number").text());
     });
+
+    //
+    // 新しい工程を追加ボタン
+    // 新しい工程を入力する欄を追加する
+    //
+     $(".add-new-process-btn").click(function(){
+
+    });
+
+
 });
 </script>
 
-<style>
 
+
+<style>
 div p{
     display: inline;
 }
