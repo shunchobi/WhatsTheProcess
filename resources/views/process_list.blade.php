6b4006e3f7ea886d381368befa159cf74e85dafe
shun <kondo@qumu.co.jp>
2022-06-17 18:34:52 +0900

    reduce space

--- resources/views/process_list.blade.php
+++ resources/views/process_list.blade.php
@@ -6,34 +6,34 @@
     <h2>{{ $purpose_title }}</h2>
 </div>
 
-        @foreach ($process_datas as $key => $value)
-                @if ($value->sort_number > 0)
-                <div class="contens-edit-part">
-                    <a class="js_edit_button" data-process-id="{{ $value->id }}">edit</a>
-                    <div class="number-block">
-                        <p class="sort-number">{{  $process_datas[$key]['sort_number'] }}</p>
-                        <div class="contents-block">
-                            <div class="code-edit">
-                                <p class="code">{{ $process_datas[$key]['command'] }}</p>
-                            </div>
-                            <p class="title">-{{ $process_datas[$key]['title'] }}-</p>
-                            <p class="description">{{ $process_datas[$key]['description'] }}</p>
-                        </div>
+@foreach ($process_datas as $key => $value)
+        @if ($value->sort_number > 0)
+        <div class="contens-edit-part">
+            <a class="js_edit_button" data-process-id="{{ $value->id }}">edit</a>
+            <div class="number-block">
+                <p class="sort-number">{{  $process_datas[$key]['sort_number'] }}</p>
+                <div class="contents-block">
+                    <div class="code-edit">
+                        <p class="code">{{ $process_datas[$key]['command'] }}</p>
                     </div>
+                    <p class="title">-{{ $process_datas[$key]['title'] }}-</p>
+                    <p class="description">{{ $process_datas[$key]['description'] }}</p>
                 </div>
-                    
-                <div id="edit-form-{{ $value->id }}" style="display: none;">
-                    <form method="post" action="{{ route('process.update', $value->id) }}">
-                        @csrf
-                        @method("PATCH")
-                        <input class="process-title" type="text" value="{{ $value->title }}" name="title">
-                        <input class="process-command" type="text" value="{{ $value->command }}" name="command">
-                        <textarea class="process-description" type="text" value="{{ $value->description }}" name="description">{{ $value->description }}</textarea>
-                        <button type="submit">Update!</button>
-                    </form>
-                </div>
-            @endif
-        @endforeach
+            </div>
+        </div>
+            
+        <div id="edit-form-{{ $value->id }}" style="display: none;">
+            <form method="post" action="{{ route('process.update', $value->id) }}">
+                @csrf
+                @method("PATCH")
+                <input class="process-title" type="text" value="{{ $value->title }}" name="title">
+                <input class="process-command" type="text" value="{{ $value->command }}" name="command">
+                <textarea class="process-description" type="text" value="{{ $value->description }}" name="description">{{ $value->description }}</textarea>
+                <button type="submit">Update!</button>
+            </form>
+        </div>
+    @endif
+@endforeach
 
 
 @endsection
